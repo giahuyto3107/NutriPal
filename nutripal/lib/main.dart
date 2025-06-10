8d62fdd1132ea99b1a663370a0fb0483270a6906
unknown <giahuyto3107@gmail.com>
2025-06-10 23:25:47 +0700

    Add logo's name and days

--- nutripal/lib/main.dart
+++ nutripal/lib/main.dart
@@ -9,12 +9,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          leading: Icon(Icons.apple),
+          title: Text("NutriPal"),
         ),
+        body: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center, 
+                children: [
+                  _dayColumn("T", "05"),
+                  _dayColumn("F", "06"),
+                  _dayColumn("S", "07"),
+                  _dayColumn("S", "08"),
+                  _dayColumn("M", "09"),
+                  _dayColumn("T", "10"),
+                  _dayColumn("W", "11"),
+                ],
+              ),
+            ],
+          )
+        ) 
+        
       ),
     );
   }
+}
+
+Widget _dayColumn(String day, String date) {
+  return Padding(
+    padding: EdgeInsets.symmetric(horizontal: 8),
+    child: Column(
+      children: [
+        Text(day),
+        Text(date),
+      ],
+    )
+  );
 }
