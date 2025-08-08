b1086378f9896ab3e920a8c774bbfce14f42cd9a
unknown <giahuyto3107@gmail.com>
2025-08-08 14:05:49 +0700

    Loading goal to recommendation page and adding kg/lbs toggle buttons

---
+++ nutripal/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:nutripal/src/features/articles/presentation/view/user_profile/gender_page.dart';
+import 'package:nutripal/src/features/articles/presentation/view/user_profile/user_profile_input.dart';
+import 'package:nutripal/src/features/articles/presentation/viewmodel/form_progress_view_model.dart';
+import 'package:nutripal/src/features/articles/presentation/viewmodel/user_profile_store.dart';
+import 'package:provider/provider.dart';
+import 'package:nutripal/src/features/articles/presentation/view/home_page.dart';
+import 'package:nutripal/src/features/articles/presentation/view/settings.dart';
+import 'package:nutripal/src/features/articles/presentation/view/analytic.dart';
+
+// class UserInputData extends ChangeNotifier {
+//   final List<UserInputData> _inputs = [];
+//   List<UserInputData> get inputs => _inputs;
+
+//   void addInput(UserInputData data) {
+//     _inputs.add(data);
+//     notifyListeners();
+//   }
+// }
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProfileStore()),
+        ChangeNotifierProvider(create: (_) => FormProgressViewModel(totalSteps: 9)),
+      ],
+        child: const MainApp(),
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      ),
+      home: Scaffold(          
+        body: SafeArea(
+          child: MultipleStepForm(),
+        ),
+      ),
+    );
+  }
+}
